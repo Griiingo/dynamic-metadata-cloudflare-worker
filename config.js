392d2bc079f3dc3d5dbfdd076aa27bc3b69fdb1c
Higor Lacerda <49889268+higorlacerda@users.noreply.github.com>
2025-04-14 16:48:04 -0300

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,19 @@
 export const config = {
-  // domainSource: "https://e3c412aa-5c74-4d02-b483-1802d4c8adc9.weweb-preview.io", Your WeWeb app preview link
-	domainSource: "https://www.griiingo.com",
-  patterns: [
-      {
-          pattern: "/empresa/[^/]+",
-          metaDataEndpoint: "https://api.griiingo.com/rest/v1/companies_metadata?slug=eq.{slug}&select=title,description,image,keywords"
-      },
-      {
-          pattern: "/evento/[^/]+",
-          metaDataEndpoint: "https://api.griiingo.com/rest/v1/events_metadata?slug=eq.{slug}"
-      },
-      {
-          pattern: "/artigo/[^/]+",
-          metaDataEndpoint: "https://api.griiingo.com/rest/v1/articles_metadata?slug=eq.{slug}"
-      }
-      // Add more patterns and their metadata endpoints as needed
-  ]
+	domainSource: "https://e3c412aa-5c74-4d02-b483-1802d4c8adc9.weweb-preview.io", // Your WeWeb app preview link
+	// domainSource: "https://www.griiingo.com",
+	patterns: [
+	      {
+	          pattern: "/empresa/[^/]+",
+	          metaDataEndpoint: "https://api.griiingo.com/rest/v1/companies_metadata?slug=eq.{slug}&select=title,description,image,keywords"
+	      },
+	      {
+	          pattern: "/evento/[^/]+",
+	          metaDataEndpoint: "https://api.griiingo.com/rest/v1/events_metadata?slug=eq.{slug}"
+	      },
+	      {
+	          pattern: "/artigo/[^/]+",
+	          metaDataEndpoint: "https://api.griiingo.com/rest/v1/articles_metadata?slug=eq.{slug}"
+	      }
+	      // Add more patterns and their metadata endpoints as needed
+	]
 };
